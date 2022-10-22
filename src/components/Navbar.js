5328628596c3b5388708ef404790aae23805d103
jbahisindra <jbahisindra@gmail.com>
2022-10-22 23:10:27 +0700

    Progress navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import { MovieFilter } from '@mui/icons-material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+
+const navItems = ['Indonesian', 'Pricing', 'About'];
+
+const Navbar = () => {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <AppBar component="nav">
+        <Toolbar>
+          <MovieFilter sx={{ display: 'flex', mr: 1 }} />
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              display: 'block',
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+            }}
+          >
+            MOVIE LIST
+          </Typography>
+          <Box sx={{ display: 'block' }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Navbar;
