e18b35672e96b515b948a448c84bbd2b6f71bb33
muhhamin <muhh.amin39@gmail.com>
2022-10-24 19:47:58 +0800

    add firebase login

---
+++ src/containers/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Box, Typography } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <>
+      <Box>
+        <Typography variant="body1">Halo, ini adalah halaman utama</Typography>
+      </Box>
+    </>
+  );
+};
+
+export default HomePage;
