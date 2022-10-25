ee1e2f6b96fcb187dc2c7ae0a9cd9fb810de062c
jbahisindra <jbahisindra@gmail.com>
2022-10-25 22:44:12 +0700

    add sort

--- src/containers/MovieList.js
+++ src/containers/MovieList.js
@@ -1,17 +1,21 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import tmdb from "../apis/tmdb";
 
 import MovieCard from "../components/MovieCard";
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
+  const [queryParams, setQueryParams] = useSearchParams();
+  const [moviesReady, setMoviesReady] = useState(false);
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
         const fetchedMovies = await tmdb.get("trending/movie/week");
         setMovies(fetchedMovies.data.results);
+        setMoviesReady(true);
       } catch (error) {
         console.log(error);
       }
@@ -19,18 +23,77 @@ const MovieList = () => {
     fetchMovies();
   }, []);
 
+  useEffect(() => {
+    if (!moviesReady) return;
+    const sortMovies = (type) => {
+      if (type === "asc") {
+        const sorted = [...movies].sort(
+          (a, b) => a.vote_average - b.vote_average
+        );
+        setMovies(sorted);
+      }
+      if (type === "desc") {
+        const sorted = [...movies].sort(
+          (a, b) => b.vote_average - a.vote_average
+        );
+        setMovies(sorted);
+      }
+    };
+
+    const sortValueFromQueryParam = queryParams.get('sort');
+    sortMovies(sortValueFromQueryParam);
+  }, [queryParams, moviesReady]);
+
+  const setSortParam = (type) => {
+    queryParams.set("sort", type);
+    setQueryParams(queryParams);
+  };
+
   return (
     <Box
       sx={{
         display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "space-between",
-        mt: 0,
-      }}>
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie}></MovieCard>
-      ))}
+        flexDirection: "column",
+        mt: 5,
+      }}
+    >
+      <Box
+        sx={{
+          mt: 5,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-end",
+          alignItems: "center",
+        }}
+      >
+        Sort by Rating
+        <Button
+          variant="contained"
+          sx={{ ml: 2 }}
+          onClick={() => setSortParam("asc")}
+        >
+          Asc
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ ml: 2, mr: 2 }}
+          onClick={() => setSortParam("desc")}
+        >
+          Desc
+        </Button>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}
+      >
+        {movies.map((movie) => (
+          <MovieCard key={movie.title} movie={movie}></MovieCard>
+        ))}
+      </Box>
     </Box>
   );
 };
